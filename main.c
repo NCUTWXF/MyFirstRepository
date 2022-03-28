0ee5e1ab6ef2be49e745d93c570f9db0400df6fc
NCUTWXF <49831260+NCUTWXF@users.noreply.github.com>
2022-03-28 17:10:41 +0800

    添加函数

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdint.h>
+
+int main(int argv,char *argc[])
+{
+    uint8_t out = 0;
+
+    return out;
+}
+
+uint8_t alg_min(uint8_t buf[],uint8_t len)
+{
+    uint8_t out =0;
+    return out;
+}
