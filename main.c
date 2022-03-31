10935125a527b65cc352cec972dac908ae222c17
NCUTWXF <49831260+NCUTWXF@users.noreply.github.com>
2022-03-31 15:41:54 +0800

    develop

--- main.c
+++ main.c
@@ -1,14 +1,33 @@
 #include <stdint.h>
+#include <stdlib.h>
 
 int main(int argv,char *argc[])
 {
     uint8_t out = 0;
-
+    if(out > 0)
+    {  
+        while( out > 10)
+        {
+            out = out + 1;
+        }
+    }
+    else
+    {   
+        while(out > 50)
+        {
+            out = out - 1;
+        }
+    }
     return out;
 }
 
 uint8_t alg_min(uint8_t buf[],uint8_t len)
 {
-    uint8_t out =0;
+    uint8_t out = 0;
+    return out;
+}
+uint8_t alg_max(uint8_t buf[],uint8_t len)
+{
+    uint8_t out = 0;
     return out;
 }
